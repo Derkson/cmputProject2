60f6b9c6254763f28783036360bac61f6c695880
arostron <alex.rostron24@gmail.com>
2019-11-16 14:07:28 -0700

    Make stuff go faster

--- phase2.sh
+++ phase2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
+sort terms.txt | perl break.pl | python3 phase2.py --prefix=terms --db_type=btree &
+sort emails.txt | perl break.pl | python3 phase2.py --prefix=emails --db_type=btree &
+sort dates.txt | perl break.pl | python3 phase2.py --prefix=dates --db_type=btree &
 sort recs.txt | perl break.pl | python3 phase2.py --prefix=recs --db_type=hash
-sort terms.txt | perl break.pl | python3 phase2.py --prefix=terms --db_type=btree
-sort emails.txt | perl break.pl | python3 phase2.py --prefix=emails --db_type=btree
-sort dates.txt | perl break.pl | python3 phase2.py --prefix=dates --db_type=btree

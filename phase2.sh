1351161c3d3fa5c005a59407fa3f03577b7e01de
arostron <alex.rostron24@gmail.com>
2019-11-20 10:22:48 -0700

    Don't use python, use db_load

--- phase2.sh
+++ phase2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-sort terms.txt | perl break.pl | python3 phase2.py --prefix=terms --db_type=btree &
-sort emails.txt | perl break.pl | python3 phase2.py --prefix=emails --db_type=btree &
-sort dates.txt | perl break.pl | python3 phase2.py --prefix=dates --db_type=btree &
-sort recs.txt | perl break.pl | python3 phase2.py --prefix=recs --db_type=hash
+sort terms.txt  | perl break.pl  | db_load -T -t btree te.idx &
+sort emails.txt | perl break.pl  | db_load -T -t btree em.idx &
+sort dates.txt  | perl break.pl  | db_load -T -t btree da.idx &
+sort recs.txt   | perl break.pl  | db_load -T -t hash  re.idx

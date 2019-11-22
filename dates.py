6d3b0f86256ad02472479b5a8e2ad1a772d059df
cjpontikes <cjpontikes@gmail.com>
2019-11-21 21:03:51 -0700

    updated the output of the process_dates_q function

--- dates.py
+++ dates.py
@@ -1,11 +1,14 @@
 import re
 
 #takes a large string, and slices out the first valid date query,
-#returns a tuple, 0th index is the remaining string, 1st index is the first valid query
+#returns a tuple, 0th index is the remaining string, 1st index another tuple
+#with 0th index being the operator and the 1st index is the date
 def process_date_q(cmd):
 	dateQuery = "(date)\s*(:|>|<|>=|<=)\s*\d{4}\/\d{2}\/\d{2}" #regex to find a valid date query
 	matcher = re.search(dateQuery, cmd)
-	return cmd[len(matcher.group(0)):].strip(), matcher.group(0)
+	operator = re.search("(:|>|<|>=|<=)", matcher.group(0))
+	date = re.search("\d{4}\/\d{2}\/\d{2}", matcher.group(0))
+	return cmd[len(matcher.group(0)):].strip(), (operator.group(0), date.group(0))
 
 
 if __name__ == "__main__":

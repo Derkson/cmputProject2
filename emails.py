e2b5fb7a4e251a89c269622bfba74553cacd00b8
arostron <alex.rostron24@gmail.com>
2019-11-21 21:46:54 -0700

    Misc fixes for josh

--- emails.py
+++ emails.py
@@ -7,15 +7,16 @@ def process_email_q(cmd):
 	endIndex = inc1 + 1
 	while endIndex == inc1 + 1:
 		inc1 = endIndex
-		endIndex = endIndex + cmd[inc1:].find(' ')
+		endIndex = endIndex + cmd[endIndex:].find(' ')
 		pass
 
 	#Here endIndex =  the index of the first space after the email
 
 	email = cmd[colonIndex + 1:endIndex].strip()
-	number = ["to","from","cc","bcc"].index(cmd[:(colonIndex-1)].strip())
-
-	return (cmd[endIndex:].stripl() , (number , email))
+	number = ["to","from","cc","bcc"].index(cmd[:(colonIndex)].strip())
+	
+	# TODO: do we need to check validity of emails???
+	return (cmd[endIndex:].strip() , (number , email))
 
 
 if __name__ == "__main__":

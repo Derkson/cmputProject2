c8b199ae84b86bffc6d28eaf452bf6ad4bf78ec8
Joshua Derkson <32886275+Derkson@users.noreply.github.com>
2019-11-22 11:30:09 -0700

    Email's works, somehow

--- emails.py
+++ emails.py
@@ -3,18 +3,28 @@ def process_email_q(cmd):
 	#print("emails")
 	colonIndex = cmd.find(':')
 
-	inc1 = colonIndex
-	endIndex = inc1 + 1
-	while endIndex == inc1 + 1:
-		inc1 = endIndex
-		endIndex = endIndex + cmd[endIndex:].find(' ')
+	increment = colonIndex
+	endIndex = colonIndex + cmd[colonIndex:].find(' ')
+	while endIndex == increment + 1:
+		increment += 1
+		endIndex = increment + cmd[increment:].find(' ') + 1
+		print(increment,"---",endIndex)
 		pass
 
 	#Here endIndex =  the index of the first space after the email
+	print(increment, "    " , endIndex)
 
+	#regular case
 	email = cmd[colonIndex + 1:endIndex].strip()
+	if endIndex <= increment:
+		#The string end after the email term
+		endIndex = len(cmd)
+		email = cmd[colonIndex + 1:].strip()
+		pass
+
+
 	number = ["to","from","cc","bcc"].index(cmd[:(colonIndex)].strip())
-	
+
 	# TODO: do we need to check validity of emails???
 	return (cmd[endIndex:].strip() , (number , email))
 

99f0e7403cab2e4904ac8155aa6f9fca9d2e517f
Joshua Derkson <32886275+Derkson@users.noreply.github.com>
2019-11-21 21:16:34 -0700

    Made it better

--- emails.py
+++ emails.py
@@ -5,26 +5,16 @@ def process_email_q(cmd):
 
 	inc1 = colonIndex
 	endIndex = inc1 + 1
-	while endIndex = inc1 + 1:
+	while endIndex == inc1 + 1:
 		inc1 = endIndex
-		endIndex = endIndex + cmd[inc1:].find(' ');
+		endIndex = endIndex + cmd[inc1:].find(' ')
 		pass
 
 	#Here endIndex =  the index of the first space after the email
 
 	email = cmd[colonIndex + 1:endIndex].strip()
-	number = 0
-
-	if cmd.starts_with(string, "from"):
-		number = 1
-		pass
-	elif cmd.starts_with(string, "cc"):
-		number = 2
-		pass
-	elif cmd.starts_with(string, "bcc"):
-		number = 3
-		pass
-
+	number = ["to","from","cc","bcc"].index(cmd[:(colonIndex-1)])
+	
 	return (cmd[endIndex:].stripl() , (number , email))
 
 

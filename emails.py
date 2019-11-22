c53d8e1054fe73a30f444f213952547a699fac8b
arostron <alex.rostron24@gmail.com>
2019-11-22 12:16:28 -0700

    Surround with try catch, return none if failed parsing

--- emails.py
+++ emails.py
@@ -1,32 +1,36 @@
 
 def process_email_q(cmd):
 	#print("emails")
-	colonIndex = cmd.find(':')
-
-	increment = colonIndex
-	endIndex = colonIndex + cmd[colonIndex:].find(' ')
-	while endIndex == increment + 1:
-		increment += 1
-		endIndex = increment + cmd[increment:].find(' ') + 1
-		print(increment,"---",endIndex)
-		pass
-
-	#Here endIndex =  the index of the first space after the email
-	print(increment, "    " , endIndex)
-
-	#regular case
-	email = cmd[colonIndex + 1:endIndex].strip()
-	if endIndex <= increment:
-		#The string end after the email term
-		endIndex = len(cmd)
-		email = cmd[colonIndex + 1:].strip()
-		pass
-
-
-	number = ["to","from","cc","bcc"].index(cmd[:(colonIndex)].strip())
-
-	# TODO: do we need to check validity of emails???
-	return (cmd[endIndex:].strip() , (number , email))
+	try:
+		colonIndex = cmd.find(':')
+	
+		increment = colonIndex
+		endIndex = colonIndex + cmd[colonIndex:].find(' ')
+		while endIndex == increment + 1:
+			increment += 1
+			endIndex = increment + cmd[increment:].find(' ') + 1
+			#print(increment,"---",endIndex)
+			pass
+	
+		#Here endIndex =  the index of the first space after the email
+		#print(increment, "    " , endIndex)
+	
+		#regular case
+		email = cmd[colonIndex + 1:endIndex].strip()
+		if endIndex <= increment:
+			#The string end after the email term
+			endIndex = len(cmd)
+			email = cmd[colonIndex + 1:].strip()
+			pass
+	
+	
+		number = ["to","from","cc","bcc"].index(cmd[:(colonIndex)].strip())
+	
+		# TODO: do we need to check validity of emails???
+		return (cmd[endIndex:].strip() , (number , email))
+	except Exception as e:
+		# something went wrong.... 
+		return cmd, None
 
 
 if __name__ == "__main__":

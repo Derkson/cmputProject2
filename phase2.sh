429e9a0e263fdae32c760d66bb050cb707aff441
arostron <alex.rostron24@gmail.com>
2019-11-23 11:26:25 -0700

    Real bash parallel

--- phase2.sh
+++ phase2.sh
@@ -3,4 +3,5 @@
 sort terms.txt  | perl break.pl  | db_load -T -t btree te.idx &
 sort emails.txt | perl break.pl  | db_load -T -t btree em.idx &
 sort dates.txt  | perl break.pl  | db_load -T -t btree da.idx &
-sort recs.txt   | perl break.pl  | db_load -T -t hash  re.idx
+sort recs.txt   | perl break.pl  | db_load -T -t hash  re.idx &
+wait

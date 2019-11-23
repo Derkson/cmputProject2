ae5777aa7436bb656d75cceebb9efba191e26d06
Joshua Derkson <32886275+Derkson@users.noreply.github.com>
2019-11-23 14:23:31 -0700

    Your toes, hand them over

--- phase3/dates.py
+++ phase3/dates.py
@@ -17,10 +17,47 @@ def process_date_q(cmd):
 
 def get_date_range(l,u,datelist):
 	# Return an upper bound and a lower bound
-	l = ""
-	pass
+	for tuple in datelist:
+		if tuple[0] == ':':
+			if ((tuple[1] >= l) and (tuple[1] <= u)):
+				l, u = tuple[1], tuple[1]
+				continue
+			return None, None
+
+		if tuple[1] > u:
+			if ((tuple[0] == '>') or (tuple[0] == '>=')):
+				return None, None
+			continue
+		if tuple[1] < l:
+			if ((tuple[0] == '<') or (tuple[0] == '<=')):
+				return None, None
+			continue
+
+		if tuple[0] == '>=':
+			l = tuple[1]
+			continue
+		if tuple[0] == '<=':
+			u = tuple[1]
+			continue
+
+		if tuple[0] == '>':
+			if tuple[1] == u:
+				return None, None
+			l = (datetime.strptime(tuple[1], "%Y/%m/%d") + timedelta(days=1)).strftime('%Y/%m/%d')
+			continue
+		if tuple[0] == '<':
+			if tuple[1] == l:
+				return None, None
+			u = (datetime.strptime(tuple[1], "%Y/%m/%d") + timedelta(days=-1)).strftime('%Y/%m/%d')
+			continue
+
+	return l, u
 
 def get_date_rows(datelist):
+
+	if datelist.isEmpty():
+		return set()
+
 	# i will get you your databse lower and upperBound
 	l, u = get_date_range(l,u,datelist)
 	if l == None:
@@ -33,8 +70,37 @@ def get_date_rows_range(lowerBound, upperBound):
 
 if __name__ == "__main__":
 	#pass
-	print(process_date_q("date>9999/12/22		yeet 	date:1233/33/21"))
-	print(process_date_q("date>1923/44/23   date:3455/12/56"))
-	print(process_date_q("date<4444/69/69 YYEEET BITHC TITSTE"))
-	print(process_date_q("DATE/4/213/3"))
+	#print(process_date_q("date>9999/12/22		yeet 	date:1233/33/21"))
+	#print(process_date_q("date>1923/44/23   date:3455/12/56"))
+	#print(process_date_q("date<4444/69/69 YYEEET BITHC TITSTE"))
+	#print(process_date_q("DATE/4/213/3"))
 	#print("Testing dates...")
+	lowerBound = "2010/01/01"
+	upperBound = "2020/01/01"
+	print(get_date_range(lowerBound,upperBound,[(":","2009/04/21")]))
+	print(get_date_range(lowerBound,upperBound,[(":","2017/04/21")]))
+	print(get_date_range(lowerBound,upperBound,[(":","2022/04/21")]))
+	print(get_date_range(lowerBound,upperBound,[(">","2009/04/21")]))
+	print(get_date_range(lowerBound,upperBound,[(">","2010/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[(">","2013/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[(">","2020/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[(">","2020/01/02")]))
+	print(get_date_range(lowerBound,upperBound,[(">=","2009/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[(">=","2010/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[(">=","2012/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[(">=","2020/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[(">=","2021/01/01")]))
+	print("lalalalalallalalala")
+	print(get_date_range(lowerBound,upperBound,[("<","2009/04/21")]))
+	print(get_date_range(lowerBound,upperBound,[("<","2010/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[("<","2013/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[("<","2020/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[("<","2020/01/02")]))
+	print(get_date_range(lowerBound,upperBound,[("<=","2009/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[("<=","2010/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[("<=","2012/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[("<=","2020/01/01")]))
+	print(get_date_range(lowerBound,upperBound,[("<=","2021/01/01")]))
+	print("BILBO BAGGY SHORTS")
+	print(get_date_range(lowerBound,upperBound,[("<=","2016/01/01"),(">","2013/04/15")]))
+	print(get_date_range(lowerBound,upperBound,[(":","2016/01/01"),(">","2016/01/01")]))

258d76fc3a34b038c1af6b4d197112a406f87133
Joshua Derkson <32886275+Derkson@users.noreply.github.com>
2019-11-23 11:26:19 -0700

    Git status

--- emails.py
+++ emails.py
@@ -3,7 +3,7 @@ def process_email_q(cmd):
 	#print("emails")
 	try:
 		colonIndex = cmd.find(':')
-	
+
 		increment = colonIndex
 		endIndex = colonIndex + cmd[colonIndex:].find(' ')
 		while endIndex == increment + 1:
@@ -11,10 +11,10 @@ def process_email_q(cmd):
 			endIndex = increment + cmd[increment:].find(' ') + 1
 			#print(increment,"---",endIndex)
 			pass
-	
+
 		#Here endIndex =  the index of the first space after the email
 		#print(increment, "    " , endIndex)
-	
+
 		#regular case
 		email = cmd[colonIndex + 1:endIndex].strip()
 		if endIndex <= increment:
@@ -22,15 +22,18 @@ def process_email_q(cmd):
 			endIndex = len(cmd)
 			email = cmd[colonIndex + 1:].strip()
 			pass
-	
-	
+
+
 		number = ["to","from","cc","bcc"].index(cmd[:(colonIndex)].strip())
-	
+
 		# TODO: do we need to check validity of emails???
 		return (cmd[endIndex:].strip() , (number , email))
 	except Exception as e:
-		# something went wrong.... 
+		# something went wrong....
 		return cmd, None
+
+def get_email_rows(eList):
+	pass
 
 
 if __name__ == "__main__":

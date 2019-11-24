c27a0f87f50c01518f8d5fc982299ec9e4655b1b
arostron <alex.rostron24@gmail.com>
2019-11-23 21:29:32 -0700

    Allow duplicates in database

--- phase2/phase2.sh
+++ phase2/phase2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-sort terms.txt  | perl break.pl  | db_load -T -t btree te.idx &
-sort emails.txt | perl break.pl  | db_load -T -t btree em.idx &
-sort dates.txt  | perl break.pl  | db_load -T -t btree da.idx &
-sort recs.txt   | perl break.pl  | db_load -T -t hash  re.idx &
+sort -u terms.txt  | perl break.pl  | db_load -c duplicates=1 -T -t btree te.idx &
+sort -u emails.txt | perl break.pl  | db_load -c duplicates=1 -T -t btree em.idx &
+sort -u dates.txt  | perl break.pl  | db_load -c duplicates=1 -T -t btree da.idx &
+sort -u recs.txt   | perl break.pl  | db_load -T -t hash  re.idx &
 wait
